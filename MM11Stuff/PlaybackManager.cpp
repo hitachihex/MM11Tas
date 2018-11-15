5385132847be3cb287d7a4065d0a7a62455a44f3
hitachihex <hitachihex@gmail.com>
2018-11-15 08:47:05 -0600

    DebugOutputs, to check for issues with DoPlayback in XInputGetState_Hook.

--- MM11Stuff/PlaybackManager.cpp
+++ MM11Stuff/PlaybackManager.cpp
@@ -31,9 +31,11 @@ unsigned long WINAPI XInputGetState_Hook(unsigned long dwUserIndex, XINPUT_STATE
 	{
 		if (g_pPlaybackManager->IsPlayingBack() && g_bPlaybackSync)
 		{
+			DebugOutput("PlayingBack, and userIndex=%u", dwUserIndex);
 			g_pPlaybackManager->DoPlayback(false, pInputState);
 			g_bPlaybackSync = false;
 
+			DebugOutput("DoPlayback success.");
 			return ERROR_SUCCESS;
 		}
 		else
@@ -45,7 +47,10 @@ unsigned long WINAPI XInputGetState_Hook(unsigned long dwUserIndex, XINPUT_STATE
 			VirtualProtect((LPVOID)&original_XInputGetState, 0x8, PAGE_EXECUTE_READWRITE, &dwOldProt);
 
 			if (*(unsigned long long*)(original_XInputGetState) == 0x0)
+			{
+				DebugOutput("original_XInputGetState is a null pointer.");
 				return ERROR_SUCCESS;
+			}
 
 			return original_XInputGetState(dwUserIndex, pInputState);
 		}
